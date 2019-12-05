17c522faf8195fa1f43278db169e00c33549b97e
XPS-13 <chen.k.yam@gmail.com>
2019-12-05 01:20:44 -0500

    added 4th example

--- Templates/FunctionTemplates.h
+++ Templates/FunctionTemplates.h
@@ -2,17 +2,26 @@
 
 template <typename T>
 void func_template1(T& con) {
-	for (const auto&c : con)
+	for (const auto& c : con)
 		std::cout << c << std::endl;
 }
 
 template <typename T1, typename T2>
 void func_template2(T1 a, T2 b) {
-	std::cout << "Function Template 2" << std::endl;
+	for (const auto& i: a){
+		for (const auto& j : b)
+			std::cout << i << " " << j << std::endl;
+	}
 }
 
-template <template<typename, typename> class V, typename T, typename Alloc>
+template <template<typename, typename> typename V, typename T, typename Alloc>
 void func_template3(V<T, Alloc>& con) {
-	for (const auto&c : con)
+	for (const auto& c : con)
+		std::cout << c << std::endl;
+}
+
+template <template<typename, typename> typename V, typename T>
+void func_template4(V<T, std::allocator<T>>& con) {
+	for (const auto& c : con)
 		std::cout << c << std::endl;
 }
--- Templates/main.cpp
+++ Templates/main.cpp
@@ -2,14 +2,25 @@
 #include <iostream>
 #include <vector>
 #include "FunctionTemplates.h"
+std::vector<int> func1(std::vector<int>& a) {
+	return a; // return type std::vector<int, std::allocator<int>> &
+}
 
+std::vector<int> func2(std::vector<int> a) {
+	return a; // return type std::vector<int, std::allocator<int>> &
+}
 int main() {
 	std::vector<int> i{ 1, 2, 3 };
 	std::vector<char> j { 'A', 'B', 'C' };
 	int k = 1;
+	func1(i);
+	func2(i);
 	func_template1(i); 
+	func_template2(i, j);
 	// func_template1(k); // won't compile
 	func_template3(j);
+
+	func_template4(j);
 
 
 
